70d6d4a29bfccf8cb0322e9e619450834b12021f
Dan McCune <dmccune@hivelocity.net>
2017-07-26 12:44:13 -0400

    adding api

--- src/Msgs.elm
+++ src/Msgs.elm
@@ -1,7 +1,10 @@
 module Msgs exposing (..)
 
+import Msgs exposing (Player)
+import RemoteData exposing (WebData)
+
 -- MESSAGES
 
 
 type Msg
-    = NoOp
+    = OnFetchPlayers (WebData (List Player))
